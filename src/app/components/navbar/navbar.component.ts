4fd931d3d762bba6fa0ced0ee8426723b2463cdb
cabe <cabe0320@gmail.com>
2024-10-01 17:49:36 -0500

    Acctualizacion modo responsivo

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -5,8 +5,18 @@ import { Component } from '@angular/core';
   standalone: true,
   imports: [],
   templateUrl: './navbar.component.html',
-  styleUrl: './navbar.component.css'
+  styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent {
+  
+  // Función para alternar el menú de navegación
+  toggleMenu() {
+    const menu = document.querySelector('.menu') as HTMLElement;
+    const toggleIcon = document.querySelector('.menu-toggle') as HTMLElement;
 
+    if (menu && toggleIcon) {
+      menu.classList.toggle('show');
+      toggleIcon.classList.toggle('active');
+    }
+  }
 }
